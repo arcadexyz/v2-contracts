727042b809f8bad3edb7b87ccc80f4eb96480b18
Kevin Kennis <kevinvkennis@gmail.com>
2022-05-11 14:44:41 -0700

    feat(docs): do fullInterestAmountCalc

--- contracts/interfaces/IFullInterestAmountCalc.sol
+++ contracts/interfaces/IFullInterestAmountCalc.sol
@@ -1,18 +1,9 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.11;
+pragma solidity ^0.8.11;
 
-/**
- * @dev Interface for a calculating the interest amount given a interest rate and principal amount.
- */
 interface IFullInterestAmountCalc {
-    /**
-     * @notice Calculate the interest due.
-     *
-     * @dev Interest and principal must be entered as base 10**18
-     *
-     * @param principal                  Principal amount in the loan terms
-     * @param interestRate               Interest rate in the loan terms
-     */
+    // ================ View Functions ================
+
     function getFullInterestAmount(uint256 principal, uint256 interestRate) external returns (uint256);
 }
