aee7925120e4a79d67f6a85e0472784d8cca603d
Kevin Kennis <kevinvkennis@gmail.com>
2022-05-11 14:32:44 -0700

    feat(docs): do permit contracts

--- contracts/interfaces/IERC721PermitUpgradeable.sol
+++ contracts/interfaces/IERC721PermitUpgradeable.sol
@@ -4,34 +4,9 @@ pragma solidity ^0.8.11;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721Upgradeable.sol";
 
-/**
- * @dev Interface for a permittable ERC721 contract
- * See https://eips.ethereum.org/EIPS/eip-2612[EIP-2612].
- *
- * Adds the {permit} method, which can be used to change an account's ERC72 allowance (see {IERC721-allowance}) by
- * presenting a message signed by the account. By not relying on {IERC721-approve}, the token holder account doesn't
- * need to send a transaction, and thus is not required to hold Ether at all.
- */
 interface IERC721PermitUpgradeable is IERC721Upgradeable {
-    /**
-     * @dev Allows `spender` to spend `tokenID` which is owned by`owner`,
-     * given ``owner``'s signed approval.
-     *
-     * Emits an {Approval} event.
-     *
-     * Requirements:
-     *
-     * - `spender` cannot be the zero address.
-     * - `owner` must be the owner of `tokenId`.
-     * - `deadline` must be a timestamp in the future.
-     * - `v`, `r` and `s` must be a valid `secp256k1` signature from `owner`
-     * over the EIP712-formatted function arguments.
-     * - the signature must use ``owner``'s current nonce (see {nonces}).
-     *
-     * For more information on the signature format, see the
-     * https://eips.ethereum.org/EIPS/eip-2612#specification[relevant EIP
-     * section].
-     */
+    // ================ Permit Functionality ================
+
     function permit(
         address owner,
         address spender,
@@ -42,18 +17,9 @@ interface IERC721PermitUpgradeable is IERC721Upgradeable {
         bytes32 s
     ) external;
 
-    /**
-     * @dev Returns the current nonce for `owner`. This value must be
-     * included whenever a signature is generated for {permit}.
-     *
-     * Every successful call to {permit} increases ``owner``'s nonce by one. This
-     * prevents a signature from being used multiple times.
-     */
-    function nonces(address owner) external view returns (uint256);
+    // ================ View Functions ================
 
-    /**
-     * @dev Returns the domain separator used in the encoding of the signature for {permit}, as defined by {EIP712}.
-     */
+    function nonces(address owner) external view returns (uint256);
     // solhint-disable-next-line func-name-mixedcase
     function DOMAIN_SEPARATOR() external view returns (bytes32);
 }
