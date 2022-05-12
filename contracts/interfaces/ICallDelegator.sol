c70349d4db66b12eab8fef681e0263793cfc597c
Kevin Kennis <kevinvkennis@gmail.com>
2022-05-12 08:16:11 -0700

    chore(docs): add final doc change

--- contracts/interfaces/ICallDelegator.sol
+++ contracts/interfaces/ICallDelegator.sol
@@ -3,5 +3,7 @@
 pragma solidity ^0.8.11;
 
 interface ICallDelegator {
+    // ============== View Functions ==============
+
     function canCallOn(address caller, address vault) external view returns (bool);
 }
