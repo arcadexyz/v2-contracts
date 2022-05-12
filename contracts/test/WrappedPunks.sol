be37957795a5d716865da9a28ab597589ec0ea25
Kevin Kennis <kevinvkennis@gmail.com>
2022-05-12 09:29:05 -0700

    chore(docs): get compiling after rebase

--- contracts/test/WrappedPunks.sol
+++ contracts/test/WrappedPunks.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.11;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "../interfaces/IPunks.sol";
+import "../external/interfaces/IPunks.sol";
 import "./UserProxy.sol";
 
 contract WrappedPunk is ERC721 {
