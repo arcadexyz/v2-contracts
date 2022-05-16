3c3fad43270f9c98cab6071471dd5ecc7a26e500
Kevin Kennis <kevinvkennis@gmail.com>
2022-05-16 09:37:49 -0700

    feat(rc): fix references and repay controller errors

--- contracts/interfaces/IRepaymentController.sol
+++ contracts/interfaces/IRepaymentController.sol
@@ -5,23 +5,22 @@ pragma solidity ^0.8.11;
 interface IRepaymentController {
     // ============== Lifeycle Operations ==============
 
-    function repay(uint256 borrowerNoteId) external;
-    function claim(uint256 lenderNoteId) external;
-    function repayPartMinimum(uint256 borrowerNoteId) external;
-    function repayPart(uint256 borrowerNoteId, uint256 amount) external;
-    function closeLoan(uint256 borrowerNoteId) external;
+    function repay(uint256 loanId) external;
+    function claim(uint256 loanId) external;
+    function repayPartMinimum(uint256 loanId) external;
+    function repayPart(uint256 loanId, uint256 amount) external;
+    function closeLoan(uint256 loanId) external;
 
     // ============== View Functions ==============
 
-    function getInstallmentMinPayment(uint256 borrowerNoteId)
+    function getInstallmentMinPayment(uint256 loanId)
         external
         view
         returns (
             uint256,
             uint256,
-            uint256,
             uint256
         );
 
-    function amountToCloseLoan(uint256 borrowerNoteId) external returns (uint256, uint256);
+    function amountToCloseLoan(uint256 loanId) external returns (uint256, uint256);
 }
