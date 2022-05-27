54bd9fd5efe96cf7517f0100c78005cd5c4e785b
Mouzayan Delbourgo <mouzayan@arcade.xyz>
2022-05-27 15:16:40 -0400

    chore(script-fixes): addressed last round of comments

--- scripts/utils/setup-roles.ts
+++ scripts/utils/setup-roles.ts
@@ -1,7 +1,8 @@
 import { ethers } from "hardhat";
+import { Contract } from "ethers";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
-import { SECTION_SEPARATOR, SUBSECTION_SEPARATOR } from "./bootstrap-tools";
+import { SECTION_SEPARATOR } from "./bootstrap-tools";
 
 import {
     ORIGINATOR_ROLE as DEFAULT_ORIGINATOR_ROLE,
@@ -11,24 +12,35 @@ import {
 } from "./constants";
 
 export async function main (
-    VAULT_FACTORY_ADDRESS = "0x3A54241cB7801BDea625565AAcb0e873e79C0649",
-    LENDER_NOTE_ADDRESS = "0xdeaBbBe620EDF275F06E75E8fab18183389d606F",
-    BORROWER_NOTE_ADDRESS = "0xaEF48370a5f37CFb760CE44E6cbF986C4DeFF389",
-    ORIGINATION_CONTROLLER_ADDRESS = "0xFE046149bc8830d989A007E81A9D7F8A3b575F36",
-    LOAN_CORE_ADDRESS = "0xd624D1879429A606f54F48B08b56126c3Fe70049",
-    ADMIN_ADDRESS = "0x9b419fd36837558D8A3197a28a5e580AcE44f64F",
-    FEE_CONTROLLER_ADDRESS = "0xE4a1917Ebe8fd2CAFD79799C82aDAa7E81AC6D47",
-    REPAYMENT_CONTROLLER_ADDRESS = "0x23ce21bE3ebd1c86325100460D58d14a1D143E8d",
-    CALL_WHITELIST_ADDRESS = "0x8a12BB999100846B9E56aba4906762353C416952",
-    PUNK_ROUTER_ADDRESS= "0x76391cd8e269f2e8fDcf893E7F5E5781B2Fe2514",
+    signers: SignerWithAddress[],
+    factory: Contract,
+    originationController: Contract,
+    borrowerNote: Contract,
+    repaymentContoller: Contract,
+    lenderNote: Contract,
+    loanCore: Contract,
+    feeController: Contract,
+    whitelist: Contract,
+    punkRouter: Contract,
     ADMIN_ROLE = DEFAULT_ADMIN_ROLE,
     FEE_CLAIMER_ROLE = DEFAULT_FEE_CLAIMER_ROLE,
     ORIGINATOR_ROLE = DEFAULT_ORIGINATOR_ROLE,
     REPAYER_ROLE = DEFAULT_REPAYER_ROLE
 ): Promise<void> {
-    const signers: SignerWithAddress[] = await ethers.getSigners();
+    signers = await ethers.getSigners();
     const [admin, adminMultiSig] = signers;
     const deployer = admin;
+
+    const ADMIN_ADDRESS = admin.address;
+    const VAULT_FACTORY_ADDRESS = factory.address;
+    const LENDER_NOTE_ADDRESS = lenderNote.address;
+    const BORROWER_NOTE_ADDRESS = borrowerNote.address;
+    const ORIGINATION_CONTROLLER_ADDRESS = originationController.address;
+    const LOAN_CORE_ADDRESS = loanCore.address;
+    const FEE_CONTROLLER_ADDRESS = feeController.address;
+    const REPAYMENT_CONTROLLER_ADDRESS = repaymentContoller.address;
+    const CALL_WHITELIST_ADDRESS = whitelist.address;
+    const PUNK_ROUTER_ADDRESS= punkRouter.address;
 
     if (!LOAN_CORE_ADDRESS) {
         throw new Error("Must specify LOAN_CORE_ADDRESS in environment!");
@@ -44,10 +56,10 @@ export async function main (
 
     console.log(SECTION_SEPARATOR);
 
-    const loanCore = await ethers.getContractAt("LoanCore", LOAN_CORE_ADDRESS);
-    const vaultFactory = await ethers.getContractAt("VaultFactory", VAULT_FACTORY_ADDRESS);
-    const lenderNote = await ethers.getContractAt("PromissoryNote", LENDER_NOTE_ADDRESS);
-    const borrowerNote = await ethers.getContractAt("PromissoryNote", BORROWER_NOTE_ADDRESS);
+    loanCore = await ethers.getContractAt("LoanCore", LOAN_CORE_ADDRESS);
+    factory = await ethers.getContractAt("VaultFactory", VAULT_FACTORY_ADDRESS);
+    lenderNote = await ethers.getContractAt("PromissoryNote", LENDER_NOTE_ADDRESS);
+    borrowerNote = await ethers.getContractAt("PromissoryNote", BORROWER_NOTE_ADDRESS);
 
     // grant correct permissions for promissory note
     // giving to user to call PromissoryNote functions directly
@@ -68,14 +80,20 @@ export async function main (
     console.log(`loanCore has granted admin role: ${ADMIN_ROLE} to address: ${ADMIN_ADDRESS}`);
 
     // grant VaultFactory the admin role to enable authorizeUpgrade onlyRole(DEFAULT_ADMIN_ROLE)
-    const updateVaultFactoryAdmin = await vaultFactory.connect(admin).grantRole(ADMIN_ROLE, ADMIN_ADDRESS);
+    const updateVaultFactoryAdmin = await factory.connect(admin).grantRole(ADMIN_ROLE, ADMIN_ADDRESS);
     await updateVaultFactoryAdmin.wait();
 
     console.log(`vaultFactory has granted admin role: ${ADMIN_ROLE} to address: ${ADMIN_ADDRESS}`);
 
     // grant originationContoller the owner role to enable authorizeUpgrade onlyOwner
-    const updateOriginationControllerAdmin = await loanCore.connect(admin).grantRole(ORIGINATOR_ROLE, ORIGINATION_CONTROLLER_ADDRESS);
+    const updateOriginationControllerAdmin = await loanCore.connect(admin).grantRole(ADMIN_ROLE, ADMIN_ADDRESS);
     await updateOriginationControllerAdmin.wait();
+
+    console.log(`originationController has granted admin role: ${ADMIN_ROLE} to address: ${ADMIN_ADDRESS}`);
+
+    // grant originationContoller the originator role
+    const updateOriginationControllerRole = await loanCore.connect(admin).grantRole(ORIGINATOR_ROLE, ORIGINATION_CONTROLLER_ADDRESS);
+    await updateOriginationControllerRole.wait();
 
     console.log(`originationController has granted originator role: ${ORIGINATOR_ROLE} to address: ${ORIGINATION_CONTROLLER_ADDRESS}`);
 
@@ -92,12 +110,12 @@ export async function main (
 
     console.log(`loanCore has renounced admin role.`);
 
-    const renounceOriginationControllerAdmin = await loanCore.connect(admin).renounceRole(ORIGINATOR_ROLE, await deployer.getAddress());
+    const renounceOriginationControllerAdmin = await loanCore.connect(admin).renounceRole(ADMIN_ROLE, await deployer.getAddress());
     await renounceOriginationControllerAdmin.wait();
 
     console.log(`originationController has renounced originator role.`);
 
-    const renounceVaultFactoryAdmin = await vaultFactory.connect(admin).renounceRole(ADMIN_ROLE, await deployer.getAddress());
+    const renounceVaultFactoryAdmin = await factory.connect(admin).renounceRole(ADMIN_ROLE, await deployer.getAddress());
     await renounceVaultFactoryAdmin.wait();
 
     console.log(`vaultFactory has renounced admin role.`);
@@ -113,33 +131,23 @@ export async function main (
     console.log(`feeController has transferred ownership to address: ${adminMultiSig.address}`);
 
     if (CALL_WHITELIST_ADDRESS) {
-    // set CallWhiteList admin
-    const whitelist = await ethers.getContractAt("CallWhitelist", CALL_WHITELIST_ADDRESS);
-    const updateWhitelistAdmin = await whitelist.transferOwnership(adminMultiSig.address);
-    await updateWhitelistAdmin.wait();
+        // set CallWhiteList admin
+        const whitelist = await ethers.getContractAt("CallWhitelist", CALL_WHITELIST_ADDRESS);
+        const updateWhitelistAdmin = await whitelist.transferOwnership(adminMultiSig.address);
+        await updateWhitelistAdmin.wait();
     }
 
     console.log(`whitelist has transferred ownership to address: ${adminMultiSig.address}`);
 
     if (PUNK_ROUTER_ADDRESS) {
-    // set PunkRouter admin
-    const punkRouter = await ethers.getContractAt("PunkRouter", PUNK_ROUTER_ADDRESS);
-    const updatepunkRouterAdmin = await punkRouter.transferOwnership(adminMultiSig.address);
-    await updatepunkRouterAdmin.wait();
+        // set PunkRouter admin
+        const punkRouter = await ethers.getContractAt("PunkRouter", PUNK_ROUTER_ADDRESS);
+        const updatepunkRouterAdmin = await punkRouter.transferOwnership(adminMultiSig.address);
+        await updatepunkRouterAdmin.wait();
     }
 
     console.log(`punkRouter has transferred ownership to address: ${adminMultiSig.address}`);
     console.log(SECTION_SEPARATOR);
     console.log("Transferred all ownership.\n");
-}
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-if (require.main === module) {
-    main()
-        .then(() => process.exit(0))
-        .catch((error: Error) => {
-            console.error(error);
-            process.exit(1);
-        });
 }
