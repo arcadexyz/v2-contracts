480294775f8120909f4d598f3b70cb3579b54141
MaD <42501101+Mouzayan@users.noreply.github.com>
2022-12-09 19:41:46 +0000

    (l-plus-fees): added collateral sale and pay later fees + corresponding tests (#62)
    * (l-plus-fees): added collateral sale and pay later fees + corresponding tests
    
    * (l-plus-fees): test label fix

--- contracts/interfaces/IFeeController.sol
+++ contracts/interfaces/IFeeController.sol
@@ -7,6 +7,8 @@ interface IFeeController {
 
     event UpdateOriginationFee(uint256 _newFee);
     event UpdateRolloverFee(uint256 _newFee);
+    event UpdateCollateralSaleFee(uint256 _newFee);
+    event UpdatePayLaterFee(uint256 _newFee);
 
     // ================ Fee Setters =================
 
@@ -14,9 +16,17 @@ interface IFeeController {
 
     function setRolloverFee(uint256 _rolloverFee) external;
 
+    function setCollateralSaleFee(uint256 _collateralSaleFee) external;
+
+    function setPayLaterFee(uint256 _payLaterFee) external;
+
     // ================ Fee Getters =================
 
     function getOriginationFee() external view returns (uint256);
 
     function getRolloverFee() external view returns (uint256);
+
+    function getCollateralSaleFee() external view returns (uint256);
+
+    function getPayLaterFee() external view returns (uint256);
 }
